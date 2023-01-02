f62dbf1b6b9a6e7a9fef0c4a30a29274b15cdb81
noman598 <nknomankhan598@gmail.com>
2023-01-02 23:38:43 +0530

    till 10 challenges completed

--- app.js
+++ app.js
@@ -15,8 +15,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+  res.render("home",({content:homeStartingContent}));
+})
 
+app.get("/about", function(req,res){
+  res.render("about", {about_content:aboutContent});
+})
 
+app.get('/contact', function(req,res){
+  res.render("contact", {contact_content:contactContent});
+})
+app.get('/compose', function(req,res){
+  res.render("compose");
+})
+app.post('/compose', function(req,res){
+  let name = req.body.item;
+  const post = {
+    title: req.body.input,
+    textArea:req.body.text
+  };
+})
 
 
 
